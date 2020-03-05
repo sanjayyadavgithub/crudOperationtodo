44e9a523cedd85177b76fac7cadc55c486c7ba35
sanjayyadavgithub <yadavsanjaay7905@gmail.com>
2020-03-05 23:16:17 +0530

    project add

---
+++ modules/upload.js
@@ -0,0 +1,14 @@
+  const mongoose = require('mongoose');
+  mongoose.connect('mongodb://localhost:27017/employee', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+  });
+  var conn = mongoose.Collection;
+
+  var uploadSchema = new mongoose.Schema({
+      imagename: String,
+
+  });
+
+  var uploadModel = mongoose.model('uploadimage', uploadSchema);
+  module.exports = uploadModel;
